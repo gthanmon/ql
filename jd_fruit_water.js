11c3ea750827d78a67d8cdc67057e34d007403cb
Gthan <62300251+gthanmon@users.noreply.github.com>
2022-08-20 03:13:15 +0800

    Add files via upload

--- jd_fruit_water.js
+++ jd_fruit_water.js
@@ -45,6 +45,7 @@ const urlSchema = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%2
       message = '';
       subTitle = '';
       option = {};
+      await shareCodesFormat();
       await jdFruit();
     }
   }
@@ -1217,7 +1218,6 @@ function shareCodesFormat() {
     if ($.shareCodesArr[$.index - 1]) {
       newShareCodes = $.shareCodesArr[$.index - 1].split('@');
     } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
       newShareCodes = shareCodes[tempIndex].split('@');
     }
@@ -1226,7 +1226,6 @@ function shareCodesFormat() {
       // newShareCodes = newShareCodes.concat(readShareCodeRes.data || []);
       newShareCodes = [...new Set([...newShareCodes, ...(readShareCodeRes.data || [])])];
     }
-    console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify(newShareCodes)}`)
     resolve();
   })
 }
